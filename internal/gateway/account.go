08dcfcfbdeda71725c37def28238d47a7bafef3a
Alladio Bonesso <alladio_bonesso@hotmail.com>
2025-01-20 18:44:49 -0300

    Add: Account.go

---
+++ internal/gateway/account.go
@@ -0,0 +1,10 @@
+package gateway
+
+import "github.com/alladiobb/ticket/internal/entity"
+
+type AccountGateway interface {
+	Save(account *entity.Account) error
+	FindByID(id string) (*entity.Account, error)
+	FindByEmail(email string) (*entity.Account, error)
+	FindByName(name string) (*entity.Account, error)
+}
