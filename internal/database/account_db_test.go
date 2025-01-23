7a1b588abe4ea8d1fa9ef3bd3e6b642a0fbea7ae
Alladio Bonesso <alladio_bonesso@hotmail.com>
2025-01-23 17:16:45 -0300

    Fix: sql.Open, Add: TestSave and TestFindByID

--- internal/database/account_db_test.go
+++ internal/database/account_db_test.go
@@ -19,7 +19,7 @@ type AccountDBTestSuite struct {
 
 func (s *AccountDBTestSuite) SetupSuite() {
 	// db, err := sql.Open("sqlite3", "file::memory:?cache=shared")
-	db, err := sql.Open("sqlite3", "memory:")
+	db, err := sql.Open("sqlite3", ":memory:")
 	s.Nil(err)
 	s.DB = db
 
@@ -37,4 +37,27 @@ func (s *AccountDBTestSuite) TearDownSuite() {
 
 func TestAccountDBTestSuite(t *testing.T) {
 	suite.Run(t, new(AccountDBTestSuite))
+}
+
+func (s *AccountDBTestSuite) TestSave() {
+	account := entity.NewAccount(s.client)
+	err := s.accountDB.Save(account)
+	s.Nil(err)
+}
+
+func (s *AccountDBTestSuite) TestFindByID() {
+	s.DB.Exec("INSERT INTO clients (id, name, email, created_at) VALUES (?, ?, ?, ?)",
+		s.client.ID, s.client.Name, s.client.Email, s.client.CreatedAt,
+	)
+
+	account := entity.NewAccount(s.client)
+	err := s.accountDB.Save(account)
+	s.Nil(err)
+	accountDB, err := s.accountDB.FindByID(account.ID)
+	s.Nil(err)
+	s.Equal(account.ID, accountDB.ID)
+	s.Equal(account.Client.ID, accountDB.Client.ID)
+	s.Equal(account.Balance, accountDB.Balance)
+	s.Equal(account.Client.Name, accountDB.Client.Name)
+	s.Equal(account.Client.Email, accountDB.Client.Email)
 }
